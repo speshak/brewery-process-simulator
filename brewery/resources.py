b50ddab540c607fed5d338ef4a3781a8886e5a6b
Scott Peshak <scott@peshak.net>
2018-09-06 16:06:36 -0500

    Docstring improvements

--- brewery/resources.py
+++ brewery/resources.py
@@ -1,42 +1,58 @@
+"""Brewery process resources."""
 import simpy
 
 
 class WaterVessel(object):
-    """A vessel that holds water that is heated"""
+    """A vessel that holds water that is heated."""
 
     def __init__(self, env, start_temp, volume):
+        """
+        Constructor.
+
+        env - SimPy environment.
+        start_temp - Starting temperature of the vessel.
+        volume - Volume of vessel.
+        """
         self.env = env
         self.cur_temp = start_temp
         self.volume = volume
 
     def _time_until_temp(self, end_temp):
-        """Calculate the required time until the vessel is at a temparture"""
-
+        """Calculate the required time until the vessel is at a temperature."""
         # TODO: use a real model for this
         # For now assume 1deg/min rise
         return min(0, end_temp - self.cur_temp)
 
     def _time_until_boil(self):
-        """Calculate the required time until the vessel is boiling"""
-
+        """Calculate the required time until the vessel is boiling."""
         return self._time_until_temp(212)
 
     def warm_to(self, target):
+        """Warm vessel to a target temp."""
         print("Warming to %d" % target)
         return self.env.timeout(self._time_until_temp(target))
 
 
 class Kettle(WaterVessel):
-    """Representation of a boil kettle"""
+    """Representation of a boil kettle."""
 
     def __init__(self, fill_volume, start_temp):
+        """Constructor."""
         super().__init__(self, start_temp, fill_volume)
 
 
 class HERMS(WaterVessel):
-    """Representation of a HERMS mash system"""
+    """Representation of a HERMS mash system."""
 
     def __init__(self, env, hlt_fill_volume, strike_volume, start_temp):
+        """
+        Constructor.
+
+        env - SimPy environment.
+        hlt_fill_volume - The fill level of the HLT
+        strike_volume - The volume of water used for str
+        start_temp - Starting temperature of vessel/water
+        """
         super().__init__(self, env, start_temp,
                          hlt_fill_volume + strike_volume)
 
@@ -45,20 +61,32 @@ class HERMS(WaterVessel):
 
 
 class Chiller(object):
-    """Representation of a chiller"""
+    """Representation of a chiller."""
 
     def __init__(self, env):
+        """
+        Constructor.
+
+        env - SimPy Environment
+        """
         self.env = env
         self.feed_rate = 10  # rate at which liquid runs through the chiller
 
     def chill(self, volume):
+        """
+        Chill a volume of water.
+
+        volume - volume of water
+        """
         yield self.env.timeout(volume / self.feed_rate)
 
 
 class Batch(object):
-    """Representation of a batch"""
+    """Representation of a batch."""
 
     def __init__(self):
+        """Constructor."""
+        self.env = None
         self.name = None
         self.recipe_type = None
         self.batch_size = None
@@ -68,27 +96,37 @@ class Batch(object):
         self.action_log = []
 
     def _log(self, action, time):
-        """Write an event entry to the log"""
+        """Write an event entry to the log."""
         print("%s -  %s at %d" % (self.name, action, time))
         self.action_log.append({"time": time, "action": action})
 
     def brew(self, env, brewery):
-        """Simulate a brew of this batch"""
+        """
+        Simulate a brew of this batch.
+
+        env - SimPy Environment
+        """
+        self.env = env
+
         if self.recipe_type == "Extract":
-            for i in self.brew_extract(env, brewery):
+            for i in self.brew_extract(brewery):
                 yield i
         else:
-            for i in self.brew_ag(env, brewery):
+            for i in self.brew_ag(brewery):
                 yield i
 
-    def brew_ag(self, env, brewery):
-        """Simulate an all grain brew of this batch"""
+    def brew_ag(self, brewery):
+        """
+        Simulate an all grain brew of this batch.
+
+        brewery - Brewery resource
+        """
         herms_req = brewery.herms.request()
         yield herms_req
         # Python3.3 allows "yield from self.mash(env)", but that breaks
         # flake8 checking.  Right now this slightly more verbose form is
         # preferable if I can retain good linting
-        for i in self.mash(env):
+        for i in self.mash():
             yield i
 
         # Get a kettle for the sparge
@@ -96,73 +134,86 @@ class Batch(object):
         yield kettle_req
 
         # Run the sparge
-        for i in self.sparge(env):
+        for i in self.sparge():
             yield i
 
         # All done with the HERMS
         brewery.herms.release(herms_req)
 
-        for i in self.boil(env):
+        for i in self.boil():
             yield i
 
         # Get a chiller
         chiller_req = brewery.chiller.request()
         yield chiller_req
-        for i in self.chill(env):
+        for i in self.chill():
             yield i
 
         # Done with the chiller & the kettle
         brewery.kettles.release(kettle_req)
         brewery.chiller.release(chiller_req)
 
-    def brew_extract(self, env, brewery):
-        """Simulate an extract brew of this batch"""
+    def brew_extract(self, brewery):
+        """
+        Simulate an extract brew of this batch.
+
+        brewery - Brewery resource
+        """
         # Get a kettle
         kettle_req = brewery.kettles.request()
         yield kettle_req
 
-        for i in self.boil(env):
+        for i in self.boil():
             yield i
 
         # Get a chiller
         chiller_req = brewery.chiller.request()
         yield chiller_req
-        for i in self.chill(env):
+        for i in self.chill():
             yield i
 
         # Done with the chiller & the kettle
         brewery.kettles.release(kettle_req)
         brewery.chiller.release(chiller_req)
 
-    def mash(self, env):
-        """Mash the batch"""
-        self._log("mash start", env.now)
-        yield env.timeout(self.mash_time)
-        self._log("mash end", env.now)
+    def mash(self):
+        """Mash the batch."""
+        self._log("mash start", self.env.now)
+        yield self.env.timeout(self.mash_time)
+        self._log("mash end", self.env.now)
 
-    def sparge(self, env):
-        """Sparge the mash"""
-        self._log("sparge start", env.now)
-        yield env.timeout(45)
-        self._log("sparge end", env.now)
+    def sparge(self):
+        """Sparge the mash."""
+        self._log("sparge start", self.env.now)
+        yield self.env.timeout(45)
+        self._log("sparge end", self.env.now)
 
-    def boil(self, env):
-        """Boil the batch"""
-        self._log("boil start", env.now)
-        yield env.timeout(self.boil_time)
-        self._log("boil end", env.now)
+    def boil(self):
+        """Boil the batch."""
+        self._log("boil start", self.env.now)
+        yield self.env.timeout(self.boil_time)
+        self._log("boil end", self.env.now)
 
-    def chill(self, env):
-        """Chill the batch"""
-        self._log("chill start", env.now)
-        yield env.timeout(self.boil_time)
-        self._log("chill end", env.now)
+    def chill(self):
+        """Chill the batch."""
+        self._log("chill start", self.env.now)
+        yield self.env.timeout(self.boil_time)
+        self._log("chill end", self.env.now)
 
 
 class Brewery(object):
-    """A representation of the brewery"""
+    """
+    A representation of the brewery.
+
+    This mainly is a container to hold the resources used by batches.
+    """
 
     def __init__(self, env):
+        """
+        Constructor.
+
+        env - SimPy environment
+        """
         self.env = env
         self.kettles = simpy.Resource(env, 2)
         self.herms = simpy.Resource(env, 1)
