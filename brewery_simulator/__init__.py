3eb2842aa5299f34ac22d3313c213a4b4e83402c
Scott Peshak <scott@peshak.net>
2018-09-09 15:50:19 -0500

    Move run_simulation to __init__
    This leaves only CLI related code in __main__, as it should be.

--- brewery_simulator/__init__.py
+++ brewery_simulator/__init__.py
@@ -1,1 +1,37 @@
 """Brewery Simulator Module."""
+import itertools
+from itertools import chain
+
+import simpy
+
+from .api import BrewDayLoader
+from .resources import Brewery
+
+
+def run_simulation(date, permutate=False):
+    """Run simulation."""
+    batches = BrewDayLoader().load_day(date)
+
+    if permutate:
+        batch_arrangements = itertools.permutations(batches)
+    else:
+        batch_arrangements = [batches]
+
+    for batches in batch_arrangements:
+        env = simpy.Environment()
+        system = Brewery(env, 2)
+
+        for batch in batches:
+            env.process(batch.brew(env, system))
+
+        env.run()
+
+        # Collate the log
+        full_log = {
+            "actions": list(chain.from_iterable(
+                [i.action_log for i in batches])),
+            "resources": list(chain.from_iterable(
+                [i.resource_log for i in batches])),
+        }
+
+        return full_log
--- brewery_simulator/__main__.py
+++ brewery_simulator/__main__.py
@@ -9,16 +9,13 @@ Options:
     date          An ISO8601 formatted date
     --permutate   Run the simulation in all possible orders
 """
-import itertools
 import logging
 import sys
-from itertools import chain
 
-import simpy
 from docopt import docopt
 
-from .api import BrewDayLoader
-from .resources import Brewery
+from .__init__ import run_simulation
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -36,35 +33,6 @@ def main():
 
     import pprint
     pprint.pprint(log)
-
-
-def run_simulation(date, permutate=False):
-    """Run simulation."""
-    batches = BrewDayLoader().load_day(date)
-
-    if permutate:
-        batch_arrangements = itertools.permutations(batches)
-    else:
-        batch_arrangements = [batches]
-
-    for batches in batch_arrangements:
-        env = simpy.Environment()
-        system = Brewery(env, 2)
-
-        for batch in batches:
-            env.process(batch.brew(env, system))
-
-        env.run()
-
-        # Collate the log
-        full_log = {
-            "actions": list(chain.from_iterable(
-                [i.action_log for i in batches])),
-            "resources": list(chain.from_iterable(
-                [i.resource_log for i in batches])),
-        }
-
-        return full_log
 
 
 if __name__ == '__main__':
